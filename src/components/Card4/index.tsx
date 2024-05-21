8c8a8fe063681d526617c7cbf98de6a98c4177d3
gsmalves <gustavoeu2017@gmail.com>
2024-05-20 23:40:24 -0300

    finalizando card de informações a cerca dos emprestimos

--- src/components/Card4/index.tsx
+++ src/components/Card4/index.tsx
@@ -1,40 +1,75 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import arrowChevron3 from '../../assets/ArrowChevron3.svg';
+import closeButtonSvg from '../../assets/Button-close.svg';
 import groupImage from '../../assets/Group.svg';
 import checkImage from '../../assets/check-icon.svg';
 import { useSliderValue } from '../../components/Contexts/SliderValueContext';
 import GenericCard from '../../components/GenericCard';
-import { AdditionalDiv, ArrowDiv, ExpandedContent, ImageDiv, InnerDiv, TextDiv } from './styles';
+import {
+  AdditionalDiv,
+  ArrowDiv,
+  ButtonAndTextWrapper,
+  CloseButton,
+  CloseButtonIcon,
+  CloseButtonWrapper,
+  ExpandedContentWrapper,
+  Image,
+  ImageDiv,
+  ImageWrapper,
+  InformationsWrapper,
+  InnerContent,
+  InnerDiv,
+  LeftDiv,
+  LeftDivBottom,
+  LeftDivBottomText,
+  LeftDivTop,
+  LeftDivTopText,
+  Line,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  OcultarText,
+  OuterDiv,
+  Paragraph,
+  RightDiv,
+  RightDivBottom,
+  RightDivBottomText,
+  RightDivTop,
+  RightDivTopText,
+  TextDiv
+} from './styles';
 
 interface Card {
   id: number;
   imageUrl: string;
   title: string;
   isExpanded: boolean;
+  closeButtonSvg: string;
 }
 
 const Card4: React.FC = () => {
   const { sliderValue, clickedButtonInfo } = useSliderValue();
   const history = useHistory();
   const [cards, setCards] = useState<Card[]>([
-    { id: 1, imageUrl: groupImage, title: 'SOLICITAÇÃO DE EMPRÉSTIMO 01', isExpanded: false },
-    { id: 2, imageUrl: checkImage, title: 'EMPRÉSTIMO CORRENTE 01', isExpanded: false },
-    { id: 3, imageUrl: checkImage, title: 'EMPRÉSTIMO CORRENTE 02', isExpanded: false },
-    { id: 4, imageUrl: checkImage, title: 'EMPRÉSTIMO CORRENTE 03', isExpanded: false },
-    // Adicione mais objetos conforme necessário
+    { id: 1, imageUrl: groupImage, title: 'SOLICITAÇÃO DE EMPRÉSTIMO 01', isExpanded: false, closeButtonSvg: closeButtonSvg },
+    { id: 2, imageUrl: checkImage, title: 'EMPRÉSTIMO CORRENTE 01', isExpanded: false, closeButtonSvg: closeButtonSvg },
+    { id: 3, imageUrl: checkImage, title: 'EMPRÉSTIMO CORRENTE 02', isExpanded: false, closeButtonSvg: closeButtonSvg },
+    { id: 4, imageUrl: checkImage, title: 'EMPRÉSTIMO CORRENTE 03', isExpanded: false, closeButtonSvg: closeButtonSvg },
   ]);
+
+  const [selectedCard, setSelectedCard] = useState<Card | null>(null);
 
   const handleBackButtonClick = () => {
     history.push('/card2');
   };
 
-  const handleArrowClick = (cardId: number) => {
-    setCards(prevCards =>
-      prevCards.map(card =>
-        card.id === cardId ? { ...card, isExpanded: !card.isExpanded } : card
-      )
-    );
+  const handleArrowClick = (card: Card) => {
+    setSelectedCard(card);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedCard(null);
   };
 
   return (
@@ -54,17 +89,92 @@ const Card4: React.FC = () => {
             <InnerDiv>
               <ImageDiv imageUrl={card.imageUrl} />
               <TextDiv>{card.title}</TextDiv>
-              <ArrowDiv onClick={() => handleArrowClick(card.id)}>
+              <ArrowDiv onClick={() => handleArrowClick(card)}>
                 <img src={arrowChevron3} alt="Expandir" />
               </ArrowDiv>
             </InnerDiv>
-            <ExpandedContent isExpanded={card.isExpanded}>
-              {/* Conteúdo expandido vai aqui */}
-              <p>Detalhes adicionais do empréstimo {card.id}...</p>
-            </ExpandedContent>
           </React.Fragment>
         ))}
       </AdditionalDiv>
+
+      {selectedCard && (
+        <ModalOverlay>
+          <Modal>
+            <ModalContent>
+              <InnerDiv>
+                <ImageDiv imageUrl={selectedCard.imageUrl} />
+                <TextDiv>{selectedCard.title}</TextDiv>
+              </InnerDiv>
+              <Line/>
+              <ExpandedContentWrapper>
+                <InnerContent>
+                  <ImageWrapper>
+                    <Image imageUrl={selectedCard.imageUrl} />
+                  </ImageWrapper>
+                  <Paragraph>Crédito Aprovado</Paragraph>
+                </InnerContent>
+                <ButtonAndTextWrapper>
+                  <CloseButtonWrapper>
+                    <CloseButton onClick={handleCloseModal}>
+                      <CloseButtonIcon src={selectedCard.closeButtonSvg} alt="Botão Fechar" />
+                    </CloseButton>
+                  </CloseButtonWrapper>
+                  <OcultarText>Ocultar</OcultarText>
+                  </ButtonAndTextWrapper>
+              </ExpandedContentWrapper>
+              <InformationsWrapper>
+              <OuterDiv>
+                    <LeftDiv>
+                      <LeftDivTop>
+                        <LeftDivTopText>Empresa</LeftDivTopText>
+                      </LeftDivTop>
+                      <LeftDivBottom>
+                        <LeftDivBottomText>Seguros Seguradora</LeftDivBottomText>
+                      </LeftDivBottom>
+                    </LeftDiv>
+                    <RightDiv>
+                      <RightDivTop>
+                        <RightDivTopText>Data de Vencimento</RightDivTopText>
+                      </RightDivTop>
+                      <RightDivBottom>
+                        <RightDivBottomText>29/06/2024</RightDivBottomText>
+                      </RightDivBottom>
+                    </RightDiv>
+                  </OuterDiv>
+                  <OuterDiv>
+                    <LeftDiv>
+                      <LeftDivTop>
+                        <LeftDivTopText>Numéros de  Parcelas</LeftDivTopText>
+                      </LeftDivTop>
+                      <LeftDivBottom>
+                        <LeftDivBottomText>2x</LeftDivBottomText>
+                      </LeftDivBottom>
+                    </LeftDiv>
+                    <RightDiv>
+                      <RightDivTop>
+                        <RightDivTopText>Valor da Parcela</RightDivTopText>
+                      </RightDivTop>
+                      <RightDivBottom>
+                        <RightDivBottomText>R$ 500</RightDivBottomText>
+                      </RightDivBottom>
+                    </RightDiv>
+                  </OuterDiv>
+                  <OuterDiv>
+                    <LeftDiv>
+                      <LeftDivTop>
+                        <LeftDivTopText>Total Financiado</LeftDivTopText>
+                      </LeftDivTop>
+                      <LeftDivBottom>
+                        <LeftDivBottomText>R$ 1000</LeftDivBottomText>
+                      </LeftDivBottom>
+                    </LeftDiv>
+                    </OuterDiv>
+              </InformationsWrapper>
+
+            </ModalContent>
+          </Modal>
+        </ModalOverlay>
+      )}
     </GenericCard>
   );
 };
--- src/components/Card4/styles.ts
+++ src/components/Card4/styles.ts
@@ -1,10 +1,8 @@
-// styles.ts
-
 import styled from 'styled-components';
 
 export const AdditionalDiv = styled.div`
   position: relative; /* Adiciona posição relativa para que o ScrollBar seja relativo a este container */
-  width: 472px;
+  width: 479px;
   height: 250px;
   overflow-y: auto;
   padding: 24px 0;
@@ -14,11 +12,9 @@ export const AdditionalDiv = styled.div`
   align-items: center;
 `;
 
-
-
 export const InnerDiv = styled.div`
   width: 100%;
-  height: 44px;
+  height: 15px;
   padding: 0 8px;
   display: flex;
   align-items: center;
@@ -53,7 +49,224 @@ export const TextDiv = styled.div`
 export const ExpandedContent = styled.div`
   display: ${(props: { isExpanded: boolean }) => (props.isExpanded ? 'block' : 'none')};
   padding: 10px;
-  background-color: #f0f0f0;
   border-top: 1px solid #ccc;
   width: 100%;
+  background-color: #fff;
+`;
+
+export const ExpandedContentWrapper = styled.div`
+  width: 400px;
+  height: 50px;
+  gap: 0px;
+  justify-content: space-between;
+  display: flex;
+  background-color: #fff;
+  
+`;
+
+export const InnerContent = styled.div`
+  display: flex;
+  justify-content: space-around; /* Alterado para espaço entre os itens */
+  align-items: center; /* Alinhando os itens verticalmente */
+  width: 195px;
+  height: 40px;
+  padding: 8px 0px 0px 0px;
+  gap: 0px;
+  border-radius: 8px 0px 0px 0px;
+  background: #9af8ab29;
+`;
+
+export const Paragraph = styled.p`
+  width: 139px;
+  height: 24px;
+  gap: 0px;
+  font-family: Open Sans;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  text-align: center;
+  color: #01354e;
+
+`;
+
+export const ImageWrapper = styled.div`
+  width: 30px;
+  height: 30px;
+
+`;
+
+export const Image = styled.div<{ imageUrl: string }>`
+ width: 28px;
+  height: 28px;
+  background-image: url(${props => props.imageUrl});
+  background-size: contain;
+  background-position: center;
+`;
+
+export const CloseButtonWrapper = styled.div`
+  display: flex;
+  align-items: center; /* Alinha verticalmente o botão */
+`;
+
+export const CloseButton = styled.button`
+  width: 40px;
+  height: 44px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+export const CloseButtonIcon = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
+
+export const OcultarText = styled.div`
+  width: 44px;
+  height: 18px;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 18px;
+  text-align: left;
+  color: #535f5f;
+`;
+
+export const ButtonAndTextWrapper = styled.div`
+  width: 84px;
+  height: 44px;
+  display: flex;
+  flex-direction: row; 
+  justify-content: space-between; 
+  align-items: center;
+`;
+
+export const OuterDiv = styled.div`
+  width: 195px;
+  height: 52px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 8px;
+`;
+
+export const LeftDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const LeftDivTop = styled.div`
+  width: 180px;
+  height: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+`;
+
+export const LeftDivTopText = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  font-weight: 700;
+  color: #000000;
+`;
+
+export const LeftDivBottom = styled.div`
+  width: 180px;
+  height: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+`;
+
+export const LeftDivBottomText = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  font-weight: 700;
+  color:  #535F5F;
+;
+`;
+export const RightDiv = styled.div`
+  width: 180px;
+  height: 40px;
+  border-radius: 4px;
+`;
+
+export const RightDivTop = styled.div`
+  width: 180px;
+  height: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+`;
+
+export const RightDivTopText = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  font-weight: 700;
+  color: #000000;
+`;
+
+export const RightDivBottom = styled.div`
+  width: 180px;
+  height: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+`;
+
+export const RightDivBottomText = styled.p`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 14px;
+  font-weight: 700;
+  color: #535F5F;
+`;
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left:0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+`;
+
+
+export const Line = styled.div`
+  width: 380px;
+  height: 0;
+  border: 1px solid #BBC3C4;
+  border-width: 1px 0 0 0;
+  margin: 20px 0;
+`;
+
+export const Modal = styled.div`
+  background: white;
+  border-radius: 10px;
+  width: 400px;
+  height: 350px;
+  max-width: 90%;
+  padding: 20px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+
+export const InformationsWrapper = styled.div`
+  width: 400px;
+  height: 200px;
+  background: #fff;
+
+  padding: 10px;
+  margin-top: 10px;
+  border: 1px solid #fff;
+  border-radius: 5px;
+
 `;
